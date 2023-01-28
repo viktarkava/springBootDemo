7c55ef218f97fdac01a605e86908dea4eceac880
Viktar Kavalenka <viktar.kava@gmail.com>
2023-01-28 15:14:41 +0300

    SpringBootDemo initial commit

---
+++ demo/src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -0,0 +1,22 @@
+package com.example.demo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Person;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao{
+
+	private static List<Person> DB = new ArrayList<>();
+	
+	@Override
+	public int insertPerson(UUID id, Person person) {
+		DB.add(new Person(id, person.getName()));
+		return 1;
+	}
+
+}
